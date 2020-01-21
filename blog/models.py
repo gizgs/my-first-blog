87906d53ed094bbe3abcd93e9b4c775da9a951dd
admin <arthur.giza@gmail.com>
2020-01-21 20:03:08 +0100

    new

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,8 @@ class Post(models.Model):
     published_date = models.DateTimeField(
             blank=True, null=True)
 
+    birthday_date = models.DateTimeField()
+
 
     def publish(self):
         self.published_date = timezone.now()
